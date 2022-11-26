8db7f5c2ae7b5790db37f007c6a15d35375fbde5
qglht <quentin.guilhot@student-cs.fr>
2022-11-26 14:32:15 +0100

    cleaner pipeline

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from scoring import f1_score
 from sklearn.model_selection import train_test_split
 
 def pipeline():
+    # loading of the data
     data = Data('public/X_train.csv', 'public/y_train.csv')
+    # featuring
     featuring_process = Features(data)
     x, y = featuring_process.featuring()
+    # split
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25,random_state=109) 
+    # model
     model_builder = SVC_ECG()
-    model = model_builder.model
-    model.fit(x_train, y_train)
-    model.save('model/models/svc.sav')
-    print(f1_score(y_test, model.predict(x_test)))
+    model_builder.model.fit(x_train, y_train)
+    model_builder.save('model/models/svc.sav')
+    # prediction
+    print(f1_score(y_test, model_builder.model.predict(x_test)))
 
 pipeline()
--- model/model.py
+++ model/model.py
@@ -1,7 +1,6 @@
 import sklearn
 import pickle
 from sklearn.svm import SVC
-from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
 class SVC_ECG(SVC):
@@ -9,7 +8,7 @@ class SVC_ECG(SVC):
         self.model = self.svm_multiclass()
 
     def svm_multiclass(self):
-        return make_pipeline(SVC(C=1, kernel="rbf"))
+        return SVC(C=1, kernel="rbf")
 
     def save(self, filename):
         pickle.dump(self.model, open(filename, 'wb'))
