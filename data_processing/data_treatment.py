d26d382a062ba43ec3baca594274cb0e6c72a075
qglht <quentin.guilhot@student-cs.fr>
2022-11-28 21:34:00 +0100

    pipeline to retrieve the hearbeats

--- data_processing/data_treatment.py
+++ data_processing/data_treatment.py
@@ -2,11 +2,15 @@ import csv
 import os
 import logging
 
+from typing import List
 import biosppy.signals.ecg as ecg
 import matplotlib.pyplot as plt
+import pickle
+import json
 import numpy as np
 import pandas as pd
-
+import biosppy.signals.ecg as ecg
+import pdb
 
 def read_data(path):
     return pd.read_csv(path, index_col='id')
@@ -23,9 +27,45 @@ class Data:
         self.x = read_data(path_X)
         self.y = read_data(path_y) 
         if sampling:
-            self.x = self.x.sample(n=1000)
+            self.x = self.x.sample(n=10)
             self.y = self.y[self.y.index.isin(self.x.index)]
         print("Data successfully read")
+
+    def segment_signal(self, signal:np.array) -> List[np.array]:
+        """Retrieves the heartbeats of constant length for one signal (one sample)
+
+        Args:
+            signal (np.array): one sample containing several heartbeats
+
+        Returns:
+            List[np.array]: List of all the heartbeats of constant length
+        """
+        peaks =  ecg.engzee_segmenter(signal, 300)['rpeaks']
+        if len(peaks) >= 2:
+            beats = ecg.extract_heartbeats(signal, peaks, 300)['templates']
+            return beats
+        else:
+            return np.array([])
+
+    def all_signals(self) -> dict:
+        """Returns a dict containing the processed data
+
+        Returns:
+            dict: key = class, value = array of all the signals
+        """
+        self.all_signals = {}
+        for index, row in self.x.iterrows():
+            category = self.y.loc[index]["y"]
+            row = row.dropna().values
+            try:
+                self.all_signals[category] = np.concatenate((self.all_signals[category],self.segment_signal(row)), axis=0)
+            except KeyError:
+                self.all_signals[category] = self.segment_signal(row)
+        with open("all_signals.pickle", "wb") as fp:
+            pickle.dump(self.all_signals,fp) 
+
+    def preprocessing(self):
+        self.all_signals()
 
     
 
--- main.py
+++ main.py
@@ -6,19 +6,20 @@ from sklearn.model_selection import train_test_split
 
 def pipeline():
     # loading of the data
-    data = Data('public/X_train.csv', 'public/y_train.csv', sampling=True)
-    # featuring
-    featuring_process = Features(data)
-    x, y = featuring_process.featuring()
-    # resampling
-    ### to do
-    # split
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25,random_state=109) 
-    # model
-    model_builder = SVC_ECG()
-    model_builder.model.fit(x_train, y_train)
-    model_builder.save('model/models/svc.sav')
-    # prediction
-    print(f1_score(y_test, model_builder.model.predict(x_test)))
+    data = Data('public/X_train.csv', 'public/y_train.csv')
+    data.preprocessing()
+    # # featuring
+    # featuring_process = Features(data)
+    # x, y = featuring_process.featuring()
+    # # resampling
+    # ### to do
+    # # split
+    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25,random_state=109) 
+    # # model
+    # model_builder = SVC_ECG()
+    # model_builder.model.fit(x_train, y_train)
+    # model_builder.save('model/models/svc.sav')
+    # # prediction
+    # print(f1_score(y_test, model_builder.model.predict(x_test)))
 
 pipeline()
