60f914b820a41d00afe8d7f76b597dcba0040240
qglht <quentin.guilhot@student-cs.fr>
2022-12-02 17:15:29 +0100

    changes

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ from scoring import f1_score
 from sklearn.model_selection import train_test_split
 from keras.utils.np_utils import to_categorical
 import pdb
+import keras
+import numpy as np
+
+def transform_predictions(a):
+    idx = np.argmax(a, axis=-1)
+    a = np.zeros( a.shape )
+    a[ np.arange(a.shape[0]), idx] = 1
+    return a
+
+
 
 def pipeline():
     #loading of the data
-    data = Data("public/X_train.csv", "public/y_train.csv")
-    data.preprocessing()
-    all_signals = data.all_signals_resampled
-    # with open("all_signals_resampled.pickle", "rb") as f:
-    #     all_signals = pickle.load(f)
+    # data = Data("public/X_train.csv", "public/y_train.csv")
+    # data.preprocessing()
+    # all_signals = data.all_signals_resampled
+    with open("all_signals_resampled.pickle", "rb") as f:
+        all_signals = pickle.load(f)
     y = all_signals["label"]
     X = all_signals.drop("label", axis=1)
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25,random_state=109) 
@@ -22,7 +32,10 @@ def pipeline():
     y_test = to_categorical(y_test)
     x_train = x_train.reshape(len(x_train), x_train.shape[1],1)
     x_test = x_test.reshape(len(x_test), x_test.shape[1],1)
-    model, history = CNN1(x_train,y_train,x_test,y_test, epochs=1)
+    model, history = CNN1(x_train,y_train,x_test,y_test, epochs=10)
+    y_pred = model.predict(x_test)
+    y_pred = transform_predictions(y_pred)
+    print(f1_score(y_test, y_pred))
     pdb.set_trace()
     
     
