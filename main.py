a1384658150ca0d8cc2a800b326493bfd6b3706e
qglht <quentin.guilhot@student-cs.fr>
2022-12-04 23:05:55 +0100

    main.py

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ def pipeline_tuning():
     y_test = to_categorical(y_test)
     x_train = x_train.reshape(len(x_train), x_train.shape[1],1)
     x_test = x_test.reshape(len(x_test), x_test.shape[1],1)
-    tuner = kt.RandomSearch(HyperCNN(), objective=kt.Objective("val_f1", direction="max"))
+    tuner = kt.RandomSearch(HyperCNN())
     callbacks = [EarlyStopping(monitor='val_loss', patience=8),
             ModelCheckpoint(filepath='best_model.h5', monitor='loss', save_best_only=True), TensorBoard(log_dir="./logs")]
-    tuner.search(x_train, y_train, epochs=10, callbacks=callbacks)
+    tuner.search(x_train, y_train, epochs=1, callbacks=callbacks)
     best_model = tuner.get_best_models()[0]
     best_model.save("best_model_tuning")
     
