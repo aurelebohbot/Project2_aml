e4031fd1ea55021eda500a3d41da03d7c3da6a9d
qglht <quentin.guilhot@student-cs.fr>
2022-12-04 19:19:05 +0100

    model and main updated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pickle
 from data_processing import Data, Features
-from model import SVC_ECG, CNN1
+from model import SVC_ECG, CNN1, HyperCNN
 from scoring import f1_score
 from sklearn.model_selection import train_test_split
 from keras.utils.np_utils import to_categorical
@@ -8,6 +8,8 @@ import pdb
 import keras
 import numpy as np
 import tensorflow as tf
+import keras-tuner as kt
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 
 def transform_predictions(a):
     idx = np.argmax(a, axis=-1)
@@ -19,11 +21,11 @@ def transform_predictions(a):
 
 def pipeline():
     #loading of the data
-    data = Data("public/X_train.csv", "public/y_train.csv", sampling=True)
-    data.preprocessing()
-    all_signals = data.all_signals_resampled
-    # with open("all_signals_resampled.pickle", "rb") as f:
-    #     all_signals = pickle.load(f)
+    # data = Data("public/X_train.csv", "public/y_train.csv", sampling=True)
+    # data.preprocessing()
+    # all_signals = data.all_signals_resampled
+    with open("all_signals_resampled.pickle", "rb") as f:
+        all_signals = pickle.load(f)
     y = all_signals["label"]
     X = all_signals.drop("label", axis=1)
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.20,random_state=109) 
@@ -33,12 +35,18 @@ def pipeline():
     y_test = to_categorical(y_test)
     x_train = x_train.reshape(len(x_train), x_train.shape[1],1)
     x_test = x_test.reshape(len(x_test), x_test.shape[1],1)
-    model, history = CNN1(x_train,y_train,x_test,y_test, epochs=10, batch_size=64)
+    # model, history = CNN1(x_train,y_train,x_test,y_test, epochs=10, batch_size=64)
     # model = tf.keras.models.load_model("best_model.h5")
-    y_pred = model.predict(x_test)
-    y_pred = transform_predictions(y_pred)
-    print(f1_score(y_test, y_pred))
-    pdb.set_trace()
+    tuner = kt.RandomSearch(HyperCNN(), objective="val_f1")
+    callbacks = [EarlyStopping(monitor='val_loss', patience=8),
+            ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True), TensorBoard(log_dir="./logs")]
+    tuner.Search(x_train, y_train, epochs=10, callbacks=callbacks)
+    best_model = tuner.get_best_models()[0]
+    best_model.save("best_model_tuning")
+    # y_pred = model.predict(x_test)
+    # y_pred = transform_predictions(y_pred)
+    # print(f1_score(y_test, y_pred))
+    # pdb.set_trace()
     
     
 
--- model/model.py
+++ model/model.py
@@ -10,7 +10,8 @@ from tensorflow import keras
 from tensorflow.keras.layers import Input, Conv2D, ELU, MaxPool2D, Flatten, Dense, Dropout, BatchNormalization, Convolution1D, MaxPool1D
 from tensorflow.keras import Sequential
 from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+import keras_tuner as kt
 
 
 
@@ -55,35 +56,41 @@ def CNN1(X_train,y_train,X_test,y_test, epochs, batch_size):
     
     
     callbacks = [EarlyStopping(monitor='val_loss', patience=8),
-            ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True)]
+            ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True), TensorBoard(log_dir="./logs")]
 
     history=model.fit(X_train, y_train,epochs=epochs,callbacks=callbacks, batch_size=batch_size,validation_data=(X_test,y_test))
     model.save('best_model')
     return(model,history)
 
-'''
+class HyperCNN(kt.HyperModel):
+    def build(self, hp):
+        im_shape=(180,1)
+        inputs_cnn=Input(shape=(im_shape), name='inputs_cnn')
+        size_filters = hp.Choice("size_filters", [64,128,256])
+        size_dense = hp.Choice("size_dense", [128,256])
 
-im_shape=(X_train.shape[1],1)
-inputs_cnn=Input(shape=(im_shape), name='inputs_cnn')
-conv1_1=Convolution1D(64, (6), activation='relu', input_shape=im_shape)(inputs_cnn)
+        conv1_1=Convolution1D(size_filters, (6), activation='relu', input_shape=im_shape)(inputs_cnn)
+        conv1_1=BatchNormalization()(conv1_1)
+        pool1=MaxPool1D(pool_size=(3), strides=(2), padding="same")(conv1_1)
+        drop1 = Dropout(0.3, input_shape=im_shape)(pool1)
 
-conv1_1=BatchNormalization()(conv1_1)
-pool1=MaxPool1D(pool_size=(3), strides=(2), padding="same")(conv1_1)
-conv2_1=Convolution1D(64, (3), activation='relu', input_shape=im_shape)(pool1)
+        conv2_1=Convolution1D(size_filters, (6), activation='relu', input_shape=im_shape)(drop1)
+        conv2_1=BatchNormalization()(conv2_1)
+        pool2=MaxPool1D(pool_size=(3), strides=(2), padding="same")(conv2_1)
+        drop2 = Dropout(0.3, input_shape=im_shape)(pool2)
 
-conv2_1=BatchNormalization()(conv2_1)
-pool2=MaxPool1D(pool_size=(2), strides=(2), padding="same")(conv2_1)
-conv3_1=Convolution1D(64, (3), activation='relu', input_shape=im_shape)(pool2)
+        flatten=Flatten()(drop2)
+        dense_end2 = Dense(size_dense, activation='relu')(flatten)
+        main_output = Dense(4, activation='softmax', name='main_output')(dense_end2)
+        
+        
+        model = Model(inputs= inputs_cnn, outputs=main_output)
+        model.compile(optimizer=keras.optimizers.Adam(hp.Float('learning_rate', min_value=0.0001, max_value=0.01, sampling="log")), loss='categorical_crossentropy',metrics = [tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+        return model
 
-conv3_1=BatchNormalization()(conv3_1)
-pool3=MaxPool1D(pool_size=(2), strides=(2), padding="same")(conv3_1)
-flatten=Flatten()(pool3)
-dense_end1 = Dense(64, activation='relu')(flatten)
-dense_end2 = Dense(32, activation='relu')(dense_end1)
-main_output = Dense(5, activation='softmax', name='main_output')(dense_end2)
-
-
-model = Model(inputs= inputs_cnn, outputs=main_output)
-model.compile(optimizer='adam', loss='categorical_crossentropy',metrics = ['accuracy'])
-
-'''
+    def fit(self, hp, model, *args, **kwargs):
+        return model.fit(
+            *args,
+            batch_size=hp.Choice("batch_size", [64,128,256]),
+            **kwargs,
+        )
