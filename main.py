78fb9e703f4535bfeed8a4171eb53aa43281f3af
qglht <quentin.guilhot@student-cs.fr>
2022-12-05 00:37:44 +0100

    recall and precision

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def pipeline_tuning():
     y_test = to_categorical(y_test)
     x_train = x_train.reshape(len(x_train), x_train.shape[1],1)
     x_test = x_test.reshape(len(x_test), x_test.shape[1],1)
-    tuner = kt.RandomSearch(HyperCNN(), objective='loss')
+    tuner = kt.RandomSearch(HyperCNN(), objective=["recall", "precision"])
     callbacks = [EarlyStopping(monitor='loss', patience=8),
             ModelCheckpoint(filepath='best_model_tuning.h5', monitor='loss', save_best_only=True), TensorBoard(log_dir="./logs")]
     tuner.search(x_train, y_train, epochs=10, callbacks=callbacks)
--- model/model.py
+++ model/model.py
@@ -85,7 +85,7 @@ class HyperCNN(kt.HyperModel):
         
         
         model = Model(inputs= inputs_cnn, outputs=main_output)
-        model.compile(optimizer=keras.optimizers.Adam(hp.Float('learning_rate', min_value=0.0001, max_value=0.01, sampling="log")), loss='categorical_crossentropy',metrics = ["accuracy"])
+        model.compile(optimizer=keras.optimizers.Adam(hp.Float('learning_rate', min_value=0.0001, max_value=0.01, sampling="log")), loss='categorical_crossentropy',metrics = [tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
         return model
 
     def fit(self, hp, model, *args, **kwargs):
@@ -94,5 +94,6 @@ class HyperCNN(kt.HyperModel):
             batch_size=hp.Choice("batch_size", [64,128,256]),
             **kwargs,
         )
-        return history.history['loss']
+        print(history.history)
+        return {"precision":history.history['precision'], "recall":history.history['recall']}
         
